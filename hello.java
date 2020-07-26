fe3dafef02a56160e2a8738b44f469bacc23b729
Saptarsi Saha <saptarshi.saha0@gmail.com>
2020-07-26 20:29:58 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ import java.io.*;
 
 class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World! from Jenkins"); 
     }
 }
